41655d394034e3a6d6cefe7258ef22fe2607ede9
xiegaofeng <xiegaofeng@grandcloud.cn>
2017-06-28 14:12:24 +0800

    Add the redis commands SADD and SMEMBERS.

---
+++ redis_sets.go
@@ -0,0 +1,25 @@
+package redis
+
+// SAdd executes the redis command SADD.
+//
+// Panic if an error occurs.
+//
+// New in redis version 1.0.0.
+func (r *Redis) SAdd(key string, member string, members ...string) int64 {
+	args := make([]interface{}, len(members)+2)
+	args[0] = key
+	args[1] = member
+	for i, m := range members {
+		args[i+2] = m
+	}
+	return r.doToInt("SADD", args...)
+}
+
+// SMembers executes the redis command SMEMBERS.
+//
+// Panic if an error occurs.
+//
+// New in redis version 1.0.0.
+func (r *Redis) SMembers(key string) []string {
+	return r.doToStringSlice("SMEMBERS", key)
+}
---
+++ redis_sets_test.go
@@ -0,0 +1,17 @@
+package redis
+
+import (
+	"fmt"
+)
+
+func ExampleRedis_SAdd() {
+	r := NewRedis("redis://127.0.0.1:6379/0", 1)
+	defer r.Close()
+
+	key := "test-sadd"
+	r.SAdd(key, "1", "2", "3")
+	fmt.Println(r.SMembers(key))
+
+	// Output:
+	// [1 2 3]
+}
